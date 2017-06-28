2e32fb6fa230a5572cb638e9575c072dfa644243
Timothy Stapleton <retrocede@outlook.com>
2017-06-28 07:31:03 -0700

    initial scaffold from SimulatedGREG-electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'shac-track',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
